529b79912cde71ecc67ac76ede9d494bc1087972
개발자1 <cm41823@gmail.com>
2026-04-23 17:51:41 +0900

    t2 green

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,22 @@
 package com.ll.framework.ioc;
 
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestPostService;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ApplicationContext {
     public ApplicationContext() {
 
     }
+    private Map<String,Object> bens = new HashMap<>();
 
     public <T> T genBean(String beanName) {
+       if(beanName.equals("testPostService")){
+           TestPostRepository testPostRepository = new TestPostRepository();
+           return (T) new TestPostService(testPostRepository);
+       }
         return (T) null;
     }
 }
