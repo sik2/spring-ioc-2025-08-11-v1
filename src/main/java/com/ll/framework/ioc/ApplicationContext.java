de3573b03699d5f250ef3834296cdf7a6df59b9e
woo0218 <jungwoo9093@gmail.com>
2026-05-04 17:08:41 +0900

    t4~t6

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -2,6 +2,7 @@ package com.ll.framework.ioc;
 
 
 import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestFacadePostService;
 import com.ll.domain.testPost.testPost.service.TestPostService;
 
 import java.util.HashMap;
@@ -9,15 +10,17 @@ import java.util.Map;
 
 public class ApplicationContext {
 
-        // Map 저장소 만들기
         private Map<String, Object> beans = new HashMap<>();
 
-        // 생성자에 빈 등록하기
         public ApplicationContext() {
             TestPostRepository testPostRepository = new TestPostRepository();
             TestPostService testPostService = new TestPostService(testPostRepository);
 
+            TestFacadePostService testFacadePostService = new TestFacadePostService(testPostService, testPostRepository);
+
             beans.put("testPostService", testPostService);
+            beans.put("testPostRepository", testPostRepository);
+            beans.put("testFacadePostService", testFacadePostService);
         }
 
     public <T> T genBean(String beanName) {
