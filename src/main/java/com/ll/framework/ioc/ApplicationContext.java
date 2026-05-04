67fd3ae899c50abeb48b08b4c403b969c0d72efe
woo0218 <jungwoo9093@gmail.com>
2026-05-04 16:43:04 +0900

    t1~t3

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,26 @@
 package com.ll.framework.ioc;
 
-public class ApplicationContext {
-    public ApplicationContext() {
 
-    }
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestPostService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApplicationContext {
+
+        // Map 저장소 만들기
+        private Map<String, Object> beans = new HashMap<>();
+
+        // 생성자에 빈 등록하기
+        public ApplicationContext() {
+            TestPostRepository testPostRepository = new TestPostRepository();
+            TestPostService testPostService = new TestPostService(testPostRepository);
+
+            beans.put("testPostService", testPostService);
+        }
 
     public <T> T genBean(String beanName) {
-        return (T) null;
+        return (T) beans.get(beanName);
     }
 }
